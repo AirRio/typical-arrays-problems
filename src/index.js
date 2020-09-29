4401725073dd344aa4dacb4bcf5f3220df402568
AirRio <45617410+AirRio@users.noreply.github.com>
2020-09-29 23:46:54 +0300

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,13 @@
+exports.min = function min(array) {
+	if (!array || array.length === 0) return 0;
+	return Math.min.apply(null, array);
+}
+exports.max = function max(array) {
+	if (!array || array.length === 0) return 0;
+	return Math.max.apply(null, array);
+}
+
+exports.avg = function avg(array) {
+	if (!array || array.length === 0) return 0;
+	return array.reduce(((a, b) => a + b) / array.length);
+}
